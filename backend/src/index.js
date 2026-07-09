20b29bd7986c670ff59d5480af0e90346e8fda57
Abhishek-B16 <abhishekbadave2005@gmail.com>
2026-07-09 19:53:50 +0530

    pushing..

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,26 @@
 
 import express from 'express';
 import "dotenv/config";
-const app = express();
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express"
+import cors from 'cors';
 
-console.log(process.env.MONGO_URI);
+
+const app = express();
+app.use(clerkMiddleware);
+app.use(cors())
+app.use(express.json());
+const PORT = process.env.PORT;
+
+app.get('/', async (req, res) => {
+  res.status(200).json({ok:true});
+});
+
+
+
 
 app.listen(3000, () => {
+  connectDB();
   console.log('Server is running on port 3000');
 });
---
+++ backend/src/lib/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+    try {
+
+        const mongoUri = process.env.MONGO_URI ;
+
+        if(!mongoUri) {
+            throw new Error('MongoDB connection string is not defined in environment variables.');
+        }
+
+        const conn =await mongoose.connect(mongoUri)
+          
+        console.log('MongoDB connected', conn.connection.host);
+    }
+    catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+    
+}
+
