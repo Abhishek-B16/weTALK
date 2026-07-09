c385613ebdd613527c3d2fca827395393f9b11cf
Abhishek-B16 <abhishekbadave2005@gmail.com>
2026-07-09 22:54:15 +0530

    integrate Clerk  authentication

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+     
+     <div>
+      <h1>my app</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode="modal" />
+          <SignUpButton  mode="modal" />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+
+     </div>
+    
+  )
+}
+
+export default App
