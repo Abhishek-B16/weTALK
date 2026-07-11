8fea477129d983a1e8da6cd29ce816e6ec483737
Abhishek-B16 <abhishekbadave2005@gmail.com>
2026-07-11 15:35:08 +0530

    fix: ensure cron job starts only in production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -48,4 +48,6 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, "0.0.0.0", () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
+
+  if(process.env.NODE_ENV === "production") job.start(); // start the cron job only in production
 });
