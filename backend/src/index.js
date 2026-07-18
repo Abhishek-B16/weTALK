a87c6b9b120c2427aae1e9c1de38a99192e1dcc1
Abhishek-B16 <abhishekbadave2005@gmail.com>
2026-07-18 18:38:29 +0530

    fix: update route to use wildcard for serving static files

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,7 +51,7 @@ app.use("/api/messages", messageRoutes);
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
-  app.get("/*", (req, res, next) => {
+  app.get("*", (req, res, next) => {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
